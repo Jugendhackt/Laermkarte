4e3feb82ef1554a7d97dc378c794fb23f14406c6
JaMo <janmoritzmeyer@yahoo.com>
2021-10-10 01:14:25 +0200

    JM | python backend fetching

--- berlinerDatenRotz/main.py
+++ berlinerDatenRotz/main.py
@@ -1,10 +1,13 @@
-import requests, cv2, numpy, math
+import requests, cv2, numpy, math, json, time
 
-from PIL import Image
+from progress.bar import Bar
 
 def getBoxData(x, y):
-    r = requests.get('https://fbinter.stadt-berlin.de/fb/wms/senstadt/wmsk_07_05_01str_vbusDEN?SERVICE=WMS&VERSION=1.3.0&REQUEST=GetMap&FORMAT=image/png&TRANSPARENT=true&CACHEID=888282&LAYERS=1&WIDTH=512&HEIGHT=512&CRS=EPSG:4326&STYLES=&BBOX={}, {}, {}, {}'.format(x, y, x+0.002, y+0.002))
-    
+    try:
+        r = requests.get('https://fbinter.stadt-berlin.de/fb/wms/senstadt/wmsk_07_05_01str_vbusDEN?SERVICE=WMS&VERSION=1.3.0&REQUEST=GetMap&FORMAT=image/png&TRANSPARENT=true&CACHEID=888282&LAYERS=1&WIDTH=128&HEIGHT=128&CRS=EPSG:4326&STYLES=&BBOX={}, {}, {}, {}'.format(x, y, x+0.004, y+0.004))
+    except requests.exceptions.ConnectionError:
+        time.sleep(30.0)
+        r = requests.get('https://fbinter.stadt-berlin.de/fb/wms/senstadt/wmsk_07_05_01str_vbusDEN?SERVICE=WMS&VERSION=1.3.0&REQUEST=GetMap&FORMAT=image/png&TRANSPARENT=true&CACHEID=888282&LAYERS=1&WIDTH=128&HEIGHT=128&CRS=EPSG:4326&STYLES=&BBOX={}, {}, {}, {}'.format(x, y, x+0.004, y+0.004))
     with open(r'image.png','wb') as f:
         f.write(r.content)
 
@@ -14,14 +17,11 @@ def getBoxData(x, y):
 
     for x in range(4):
         for y in range(4):
-            imgSlices.append(myimg[x*128:x*128+128, y*128:y*128+128])
+            imgSlices.append(myimg[x*32:x*32+32, y*32:y*32+32])
 
-    for x in range(16):
-        cv2.imwrite("filename{}.png".format(x), imgSlices[x])
-        
-
-    finalSize = 16
+    # finalSize = 16
     # 128 pixels each
+    # 32 pixels each
 
     resultArray = []
 
@@ -30,13 +30,13 @@ def getBoxData(x, y):
         count = {}
         for x in imgSlices[i]:
             for y in x:
-                text = translateToDB(str('{:03}'.format(y[2]))+str('{:03}'.format(y[1]))+str('{:03}'.format(y[0])))
-                if text in count:
-                    count[text] = count[text] + 1
+                key = translateToDB(str('{:03}'.format(y[2]))+str('{:03}'.format(y[1]))+str('{:03}'.format(y[0])))
+                if key in count:
+                    count[key] = count[key] + 1
                 else:
-                    count[text] = 1
+                    count[key] = 1
         #count.pop(0)
-        countedPixels = sum(count.values())
+        countedPixels = 1024 # sum(count.values())
         result = 0.0
         for x in count.keys():
             result += x * (count[x] / countedPixels)
@@ -49,11 +49,11 @@ def getBoxData(x, y):
     # 65-70 => (138, 18, 20) 68
     # 70-75 => (145, 15, 102) 73
     # >75   => (41, 115, 184) 75
-    return resultArray
+    return resultArray #[::-1]
 
 def translateToDB(var):
     if var == "255255255":
-        return 0
+        return 50
     elif var == "255099054":
         return 58
     elif var == "199023018":
@@ -66,28 +66,47 @@ def translateToDB(var):
         return 75
 
 
-startValueX = 52.537791
-startValueY = 13.343777
-endValueX = 52.566129
-endValueY = 13.392601
+startValueX = 52.423292
+startValueY = 13.262662
 
 dbDict = {}
 
-for x in range(5):
-    for y in range(5):
+x = 40
+y = 60
+
+class FancyBar(Bar):
+    suffix = '%(percent).1f%% - %(eta)ds'
+
+bar = FancyBar('Fetching', max=2400)
+
+for x in range(x):
+    for y in range(y):
         data = getBoxData(startValueX+x*0.004, startValueY+y*0.004)
         i = 0
-        for dat in data:
-            dbDict[(startValueX+x*0.004+math.ceil(i/4)*0.0005, startValueY+y*0.004+(i%4)*0.0005)] = dat
-            i = i + 1
+        xStart = startValueX+0.004+x*0.004
+        yStart = startValueY+y*0.004
+        for xi in range(4):
+            xStart = xStart - 0.001
+            for yi in range(4):
+                dbDict[(xStart,yStart+yi*0.001)] = data[i]
+                i = i+1
+        bar.next()
+bar.finish()
 
 
-
+with open('result.json', 'w') as fp:
+    json.dump(dbDict, fp)
 
 string = "["
 for key in dbDict.keys():
     string = string + "{"+"lat: {}, lng: {}, count: {}".format(key[0], key[1], dbDict[key])+"},"
 
+string = string.rstrip(',')
+
 string = string + "]"
+
+
+with open("Output.txt", "w") as text_file:
+    text_file.write(string)
 
 print (string)
