d85c7537bc0f65893afd81a2ae7ee0b695fd2cc6
hannahne <51098196+hannahne@users.noreply.github.com>
2021-10-09 18:46:17 +0200

    changed name from sound-sensor to sensor

--- Sensor-Client/request.py
+++ Sensor-Client/request.py
@@ -2,13 +2,13 @@ import requests
 import json
 from datetime import datetime
 
-
+import sensor
 
 id = 1
 time = datetime.now(tz=None).strftime("%Y-%m-%d %H:%M")
 lat = 53
 long = 13
-val = sensor.getDB()
+val = sensor.get_current_decibels()
 var = {"identity": id, "timestamp": time, "location": {"latitude": lat, "longitude": long},
         "value": val}
 
